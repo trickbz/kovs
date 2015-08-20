ffd4755b7b65e2372398f847714e36cc517667c1
trickbz <trickbz@gmail.com>
2015-08-20 16:45:16 +0300

    initial commit

---
+++ knockout/Scripts/bootstrapper.js
@@ -0,0 +1,49 @@
+﻿require.config({
+
+    baseUrl: '.',
+    paths: {
+        // app locations
+        'view': 'js/views',
+        'model': 'js/models',
+        'collection': 'js/collections',
+
+        // frameworks
+        'jquery': 'Scripts/jquery/dist/jquery',
+        'text': '../bower_components/text/text',
+        'underscore': '../bower_components/underscore/underscore',
+        'backbone': '../bower_components/backbone/backbone'
+    },
+    shim: {
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        }
+    }
+});
+
+// main app module
+define([
+	"model/post_model",
+	"underscore",
+	"js/helpers/utils",
+	"collection/postsCollection",
+	'text!../templates/posts_table_template.html',
+	'view/postsTableView'
+], function (
+	PostModel,
+	_,
+	utils,
+	PostsCollection,
+	PostsTableTemplate,
+	PostsTableView) {
+    var posts = new PostsCollection();
+    posts.fetch({
+        success: function (collection, response, options) {
+            var compiledPostTableTemplate = _.template(PostsTableTemplate)({ posts: collection.toJSON() });
+            PostsTableView.render(compiledPostTableTemplate);
+        }
+    });
+});
---
+++ knockout/Scripts/ko/components.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="/Scripts/knockout-3.3.0.js" start-page="/index.html" />
+ko.components.register("like-widget", {
+    viewModel: function (params) {
+        var self = this;
+        this.chosenValue = params.value;
+
+        this.like = function() {
+            self.chosenValue("like");
+        };
+
+        this.dislike = function() {
+            self.chosenValue("dislike");
+        }
+    },
+    template:
+        "<div data-bind='visible: !chosenValue()'>" +
+        "   <button data-bind='click: like'>Like it</button>" +
+        "   <button data-bind='click: dislike'>Dislike it</button>" +
+        "</div>" +
+        "<div data-bind='visible: chosenValue()'>" +
+        "   You <strong data-bind='text: chosenValue'></strong> it" +
+        "</div>"
+});
