e9ec16577e22a8dd93eb0c3f89d0130fb7f3bd5a
Alexey Petrenko <apetrenko@lohika.com>
2015-08-21 08:36:40 +0300

    hren

--- knockout/Scripts/bootstrapper.js
+++ knockout/Scripts/bootstrapper.js
@@ -1,49 +1,27 @@
 ﻿require.config({
 
-    baseUrl: '.',
+    baseUrl: 'Scripts',
     paths: {
         // app locations
-        'view': 'js/views',
-        'model': 'js/models',
-        'collection': 'js/collections',
+
 
         // frameworks
-        'jquery': 'Scripts/jquery/dist/jquery',
-        'text': '../bower_components/text/text',
-        'underscore': '../bower_components/underscore/underscore',
-        'backbone': '../bower_components/backbone/backbone'
+        'jquery': 'jquery-2.1.4.min',
+        'text': 'text',
+        'knockout': 'knockout-3.3.0'
     },
-    shim: {
-        'backbone': {
-            deps: ['underscore', 'jquery'],
-            exports: 'Backbone'
-        },
-        'underscore': {
-            exports: '_'
-        }
-    }
+    shim: {}
 });
 
 // main app module
-define([
-	"model/post_model",
-	"underscore",
-	"js/helpers/utils",
-	"collection/postsCollection",
-	'text!../templates/posts_table_template.html',
-	'view/postsTableView'
-], function (
-	PostModel,
-	_,
-	utils,
-	PostsCollection,
-	PostsTableTemplate,
-	PostsTableView) {
-    var posts = new PostsCollection();
-    posts.fetch({
-        success: function (collection, response, options) {
-            var compiledPostTableTemplate = _.template(PostsTableTemplate)({ posts: collection.toJSON() });
-            PostsTableView.render(compiledPostTableTemplate);
-        }
-    });
+define(function (require) {
+    var ko = require('knockout');
+    var $ = require('jquery');
+    var domReady = require('domReady');
+    //var indexViewModel = require("indexViewModel");
+
+    //domReady(function () {
+    //    console.log($.fn.jquery);
+    //    ko.applyBindings(new indexViewModel());
+    //});
 });
---
+++ knockout/Scripts/indexViewModel.js
@@ -0,0 +1,25 @@
+﻿define(function (require) {
+    var ko = require('knockout');
+    return function viewModel() {
+        var self = this;
+
+        function Person(firstName, lastName) {
+            this.firstName = ko.observable(firstName);
+            this.lastName = ko.observable(lastName);
+        }
+
+        self.persons = ko.observableArray([
+            new Person('John', 'Lennon'),
+            new Person('John', 'Doe'),
+            new Person('George', 'Lennonist'),
+            new Person('John', 'Govno'),
+            new Person('George', 'Pizda'),
+        ]);
+
+        self.firstNames = ko.computed(function () {
+            ko.utils.arrayMap(self.persons(), function (person) {
+                return person.firstName();
+            });
+        });
+    }
+});
