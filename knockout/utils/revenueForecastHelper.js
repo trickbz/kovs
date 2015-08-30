d8666cadeaa0cab051f2a867af3276e17f76ce10
Alexey Petrenko <apetrenko@lohika.com>
2015-08-30 23:22:14 +0300

    valueByPath

--- knockout/utils/revenueForecastHelper.js
+++ knockout/utils/revenueForecastHelper.js
@@ -84,5 +84,18 @@
         return [nodeRoot];
     }
 
+    RevenueForecastHelper.prototype.valueByPath = function (obj, path) {
+        for (var i = 0, path = path.split("."), len = path.length; i < len; i++) {
+            if ($.isArray(obj[path[i]])) {
+                obj = obj[path[i]][path[i + 1]];
+                i++;
+
+            } else {
+                obj = obj[path[i]];
+            }
+        };
+        return obj;
+    };
+
     return RevenueForecastHelper;
 }).call();
